692a99da4df9e424025a84b2fb688f958f8fcb22
Dav6yh <davyh6128@gmail.com>
2025-07-21 17:44:34 -0300

    Codigos

---
+++ Assets/Scripts/Narrador.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+public class Narrador : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI armasTexto;
+    [SerializeField] private TextMeshProUGUI tensaoTexto;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
